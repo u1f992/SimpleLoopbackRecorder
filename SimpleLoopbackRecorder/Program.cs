7d7bb2b678b65a423cb5705778ba4ae9b3f07c15
Koutaro Mukai <mukai.k1011k@outlook.jp>
2021-05-22 09:54:17 +0900

    imitation

--- SimpleLoopbackRecorder/Program.cs
+++ SimpleLoopbackRecorder/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using NAudio.Wave;
 
 namespace SimpleLoopbackRecorder
 {
@@ -6,7 +10,33 @@ namespace SimpleLoopbackRecorder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
+            Directory.CreateDirectory(outputFolder);
+            var outputFilePath = Path.Combine(outputFolder, "recorded.wav");
+            var capture = new WasapiLoopbackCapture();
+            var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
+
+            capture.DataAvailable += (s, a) =>
+            {
+                writer.Write(a.Buffer, 0, a.BytesRecorded);
+                if (writer.Position > capture.WaveFormat.AverageBytesPerSecond * 20)
+                {
+                    capture.StopRecording();
+                }
+            };
+
+            capture.RecordingStopped += (s, a) =>
+            {
+                writer.Dispose();
+                writer = null;
+                capture.Dispose();
+            };
+
+            capture.StartRecording();
+            while (capture.CaptureState != NAudio.CoreAudioApi.CaptureState.Stopped)
+            {
+                Thread.Sleep(500);
+            }
         }
     }
 }
