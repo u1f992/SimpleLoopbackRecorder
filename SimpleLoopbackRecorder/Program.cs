5fdac6bdc5aa8ab0321637a1be9b5e3468d0308c
Koutaro Mukai <mukai.k1011k@outlook.jp>
2021-05-22 21:00:42 +0900

    select device by arguments

--- SimpleLoopbackRecorder/Program.cs
+++ SimpleLoopbackRecorder/Program.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 using System.IO;
 using System.Diagnostics;
-using System.Threading;
+using NAudio.CoreAudioApi;
 using NAudio.Wave;
 
 namespace SimpleLoopbackRecorder
 {
     public class Program
     {
-        private const string SLR_VERSION = "0.2";
-        private const string SLR_USEAGE = "Usage:\n\tslr {-o | --output} <filename> [Options]\nOptions:\n\t-o, --output\tspecify output\n\t-v, --version\tprint version\n\t-h, --help\tprint help";
+        private const string SLR_VERSION = "1.0";
+        private const string SLR_USEAGE = "Usage:\n\tslr {-o | --output} <filename> [Options]\nOptions:\n\t-o, --output <filename>\tspecify output\n\t-d, --device <id>\tspecify device\n\t-l, --list\t\tprint device list\n\t-v, --version\t\tprint version\n\t-h, --help\t\tprint help";
 
         static void Main(string[] args)
         {
             string outputFilePath = "";
-            if (!AnalyzeArgs(args, ref outputFilePath))
+            MMDevice _MMDevice = null;
+            if (!AnalyzeArgs(args, ref outputFilePath, ref _MMDevice))
             {
                 if (outputFilePath == "")
                 {
                     // output指定がないor不正
-                    Console.Error.WriteLine("\n{0}", SLR_USEAGE);
+                    // デバイス指定が不正
+                    Console.WriteLine("\n{0}\n", SLR_USEAGE);
                     Environment.Exit(1);
                 }
                 else
@@ -29,16 +31,8 @@ namespace SimpleLoopbackRecorder
                 }
             }
 
-            var capture = new WasapiLoopbackCapture();
+            var capture = new WasapiLoopbackCapture(_MMDevice);
             var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
-
-            // 何も再生していない時間も録音する
-            var silence = new WaveOutEvent();
-            silence.Init(new SilenceProvider(capture.WaveFormat).ToSampleProvider());
-            silence.Play();
-
-            var stopwatch = new Stopwatch();
-
             capture.DataAvailable += (s, a) =>
             {
                 writer.Write(a.Buffer, 0, a.BytesRecorded);
@@ -49,6 +43,13 @@ namespace SimpleLoopbackRecorder
                 writer = null;
                 capture.Dispose();
             };
+
+            // 何も再生していない時間も録音する
+            var silence = new WasapiOut();
+            silence.Init(new SilenceProvider(capture.WaveFormat).ToSampleProvider());
+            silence.Play();
+
+            var stopwatch = new Stopwatch();
 
             stopwatch.Start();
             capture.StartRecording();
@@ -76,40 +77,81 @@ namespace SimpleLoopbackRecorder
         /// 正常 outptFilePath != "",
         /// 中断 ret == false
         /// </returns>
-        private static bool AnalyzeArgs(string[] args, ref string outputFilePath)
+        private static bool AnalyzeArgs(string[] args, ref string outputFilePath, ref MMDevice _MMDevice)
         {
             string ret = "";
+            _MMDevice = null;
+
+            // -o, -dは一度のみ検出する
+            bool flag_output, flag_device;
+            flag_output = false;
+            flag_device = false;
 
             for (int i = 0; i < args.Length; i++)
             {
                 var tmp = args[i];
-                if ((tmp == "-o" || tmp == "--output") && i != args.Length - 1)
+                if ((tmp == "-o" || tmp == "--output") && i != args.Length - 1 && !flag_output)
                 {
+                    // 出力ファイルを指定
                     i++;
                     ret = args[i];
                     if (!ValidatePath(ref ret))
                     {
+                        Console.Error.WriteLine("Error: Invalid filename.");
                         ret = "";
+                        return false;
                     }
+                    flag_output = true;
                 }
-                else if (tmp == "-h" || tmp == "--help")
+                else if ((tmp == "-d" || tmp == "--device") && i != args.Length - 1 && !flag_device)
                 {
-                    Console.WriteLine(SLR_USEAGE);
-                    outputFilePath = "?help";
+                    // 使用するデバイスを指定
+                    i++;
+                    var deviceID = args[i];
+                    _MMDevice = GetDeviceFromID(deviceID);
+                    if (_MMDevice == null)
+                    {
+                        Console.Error.WriteLine("Error: Invalid device ID.");
+                        ret = "";
+                        return false;
+                    }
+                    flag_device = true;
+                }
+                else if ((tmp == "-o" || tmp == "--output" || tmp == "-d" || tmp == "--device") && i == args.Length - 1)
+                {
+                    // 直後に値が必要な引数が最後にある
+                    Console.Error.WriteLine("Error: Invalid arguments.");
+                    ret = "";
+                    return false;
+                }
+                else if (tmp == "-l" || tmp == "--list")
+                {
+                    // デバイス一覧を表示
+                    Console.WriteLine(GenerateDeviceList());
+                    outputFilePath = "?list";
                     return false;
                 }
                 else if (tmp == "-v" || tmp == "--version")
                 {
+                    // バージョン情報
                     Console.WriteLine("SimpleLoopbackRecorder version {0}", SLR_VERSION);
                     outputFilePath = "?version";
                     return false;
                 }
+                else if (tmp == "-h" || tmp == "--help")
+                {
+                    // ヘルプ
+                    Console.WriteLine(SLR_USEAGE);
+                    Console.WriteLine(GenerateDeviceList());
+                    outputFilePath = "?help";
+                    return false;
+                }
             }
 
-            if (ret == "")
+            if (!flag_device)
             {
-                Console.Error.WriteLine("Error: Invalid designation.");
-                return false;
+                var enumerator = new MMDeviceEnumerator();
+                _MMDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
             }
 
             outputFilePath = ret;
@@ -233,6 +275,29 @@ namespace SimpleLoopbackRecorder
                 // Path contains invalid characters
             }
             return false;
+        }
+
+        private static string GenerateDeviceList()
+        {
+            string ret = "Device List:\n\tFriendlyName :\tID\n";
+
+            var enumerator = new MMDeviceEnumerator();
+            foreach (var wasapi in enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+            {
+                ret += $"\t\"{wasapi.FriendlyName}\" : {wasapi.ID}\n";
+            }
+
+            return ret;
+        }
+
+        private static MMDevice GetDeviceFromID(string deviceID)
+        {
+            var enumerator = new MMDeviceEnumerator();
+            foreach (var wasapi in enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+            {
+                if (wasapi.ID == deviceID) return wasapi;
+            }
+            return null;
         }
     }
 }
