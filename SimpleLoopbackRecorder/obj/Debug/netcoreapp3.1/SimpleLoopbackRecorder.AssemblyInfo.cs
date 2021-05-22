a79b51b41b1396cef45f862633756d4d6110a3c1
Koutaro Mukai <mukai.k1011k@outlook.jp>
2021-05-22 10:00:14 +0900

    change the binary name

--- SimpleLoopbackRecorder/obj/Debug/netcoreapp3.1/SimpleLoopbackRecorder.AssemblyInfo.cs
+++ SimpleLoopbackRecorder/obj/Debug/netcoreapp3.1/SimpleLoopbackRecorder.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SimpleLoopbackRecorder")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("slr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("SimpleLoopbackRecorder")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SimpleLoopbackRecorder")]
+[assembly: System.Reflection.AssemblyProductAttribute("slr")]
+[assembly: System.Reflection.AssemblyTitleAttribute("slr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment クラスによって生成されました。
